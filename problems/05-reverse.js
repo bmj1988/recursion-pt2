36ba32c2d43c0d338ff9d86c87cc428760da951d
Rasheed Lindsey <rasheedlindsey@live.com>
2023-05-03 18:49:14 -0400

    05 solved

--- problems/05-reverse.js
+++ problems/05-reverse.js
@@ -14,10 +14,18 @@ reverse(""); // ""
 
 
 function reverse(str) {
-  // Your code here
+  if (str.length <= 1) return str
+  else {
+    return str[str.length -1] + reverse(str.slice(0, str.length - 1))
+  }
 }
 
-
+console.log(reverse("house")); // "esuoh"
+reverse("dog"); // "god"
+reverse("atom"); // "mota"
+reverse("q"); // "q"
+reverse("id"); // "di"
+reverse(""); // ""
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = reverse;
